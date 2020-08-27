fa6faac5d8f04d6f83d6015db6a16a39a13439b2
vivekanand <vivek2331998@gmail.com>
2020-08-27 13:45:40 +0530

    commit all

---
+++ src/leap.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class leap {
+
+	public static void main(String[] args) 
+	{
+		int year;
+		System.out.println("enter any year");
+		Scanner sc=new Scanner(System.in);
+		year =sc.nextInt();
+		boolean leap=true;
+		
+		if(year%4==0)
+		
+		{
+		System.out.println("year is leap");	
+		}
+		else if(year%4!=0) 
+		{
+			System.out.println("year is not leap");
+		}
+		
+		
+
+	}
+
+}
